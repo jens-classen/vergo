7a42ebb2f37c6398c0d515b3d9b1d8850c641a43
Jens Claßen <classen@kbsg.rwth-aachen.de>
2015-03-06 16:17:58 +0000

    Merged utils libs.

--- lib/utils.pl
+++ lib/utils.pl
@@ -4,7 +4,12 @@
            report_message/1,
            count/2,
            atom_string/2,
-           string_codes/2]).
+           string_codes/2,
+           subset/2,
+           intersection/2,
+           disjoint/2,
+           member/2,
+           forall/2]).
 
 /*  T2 is T1 with X1 replaced by X2  */
 subv(X1,X2,T1,T2) :- var(T1), T1 == X1, !, T2 = X2.
@@ -27,3 +32,33 @@ count(G, Count) :-
 /* Renaming SWI 6.4.1 -> 6.6.0 */
 atom_string(A,S) :- string_to_atom(S,A).
 string_codes(S,C) :- string_to_list(S,C).
+
+subset2([X|Xs],Ys) :-
+        member2(X,Ys),
+        subset2(Xs,Ys).
+subset2([],_Ys).
+
+intersection2([X|Xs],Ys,[X|Zs]) :-
+        member2(X,Ys), !,
+        intersection2(Xs,Ys,Zs).
+intersection2([_|Xs],Ys,Zs) :-
+        intersection2(Xs,Ys,Zs).
+intersection2([],_,[]).
+
+disjoint2([X|Xs],Ys) :-
+        not(member2(X,Ys)),
+        disjoint2(Xs,Ys).
+disjoint2([],_Ys).
+
+member2(X,[Y|_L]) :- X == Y.
+member2(X,[_|L]) :- member2(X,L).
+member2(_,[]) :- fail.
+
+
+% do Action for each Goal
+forall(Goal,Action):-
+        Goal,
+	(Action
+	->	fail
+	;	!,fail).
+forall(_,_).
