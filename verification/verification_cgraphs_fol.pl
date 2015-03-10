9726d78085f3127edf04846b7821287dd0509a0c
Jens Claßen <classen@kbsg.rwth-aachen.de>
2015-03-10 10:25:45 +0000

    Added program name argument for cgraph components.

--- verification/verification_cgraphs_fol.pl
+++ verification/verification_cgraphs_fol.pl
@@ -17,8 +17,6 @@ PhD Thesis, Department of Computer Science, RWTH Aachen University,
 :- use_module('../lib/utils').
 :- use_module('../lib/env').
 
-% todo: cg_nodes with final conditions
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % checkEX
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -44,32 +42,32 @@ PhD Thesis, Department of Computer Science, RWTH Aachen University,
 construct_characteristic_graph(ProgramName) :-
         
         % eliminate previous instances
-        retractall(cg_node(_,_,_)),
-        retractall(cg_edge(_,_,_,_,_,_)),
-        retractall(cg_number_of_nodes(_)),
+        retractall(cg_node(_,_,_,_)),
+        retractall(cg_edge(_,_,_,_,_,_,_)),
+        retractall(cg_number_of_nodes(_,_)),
         
         % find the program with the given name
         program(ProgramName,Program),
         simplify_program(Program,SimplifiedProgram),
         
         % create initial node
-        assert(cg_number_of_nodes(0)),
-        cg_get_node_id(SimplifiedProgram,0),
-        cg_draw_graph,
+        assert(cg_number_of_nodes(ProgramName,0)),
+        cg_get_node_id(ProgramName,SimplifiedProgram,0),
+        cg_draw_graph(ProgramName),
         
-        iterate_cg_construction.
+        iterate_cg_construction(ProgramName).
         
-iterate_cg_construction :-
-        cg_construction_step, 
+iterate_cg_construction(ProgramName) :-
+        cg_construction_step(ProgramName), 
         !,
-        cg_draw_graph,
-        iterate_cg_construction.
-iterate_cg_construction.
+        cg_draw_graph(ProgramName),
+        iterate_cg_construction(ProgramName).
+iterate_cg_construction(_ProgramName).
 
-cg_construction_step :-
+cg_construction_step(ProgramName) :-
         
         % there is some node
-        cg_node(Program,_Final,ID),
+        cg_node(ProgramName,Program,_Final,ID),
         
         % whose program has a possible transition
         trans(Program,Action,NewProgram,Condition1,Vars,Condition2),
@@ -78,49 +76,49 @@ cg_construction_step :-
         simplify_max(Condition2,SimplifiedCondition2),
         SimplifiedCondition2\=false,
         simplify_program(NewProgram,NewSimplifiedProgram),
-        cg_get_node_id(NewSimplifiedProgram,NewID),
+        cg_get_node_id(ProgramName,NewSimplifiedProgram,NewID),
         
         % that is not yet in the graph
-        not(cg_edge(ID,Action,NewID,SimplifiedCondition1,Vars,
-                    SimplifiedCondition2)),        
-        
+        not(cg_edge(ProgramName,ID,Action,NewID,SimplifiedCondition1,
+                    Vars,SimplifiedCondition2)),
         % then
         !,
                 
         % create a new edge.
-        assert(cg_edge(ID,Action,NewID,SimplifiedCondition1,Vars,
+        assert(cg_edge(ProgramName,ID,Action,NewID,
+                       SimplifiedCondition1,Vars,
                        SimplifiedCondition2)).
 
-cg_get_node_id(Program,ID) :-
-        cg_node(Program,_Final,ID), !.
-cg_get_node_id(Program,ID) :-
-        retract(cg_number_of_nodes(ID)),
+cg_get_node_id(ProgramName,Program,ID) :-
+        cg_node(ProgramName,Program,_Final,ID), !.
+cg_get_node_id(ProgramName,Program,ID) :-
+        retract(cg_number_of_nodes(ProgramName,ID)),
         NextID is ID+1,
-        assert(cg_number_of_nodes(NextID)),
+        assert(cg_number_of_nodes(ProgramName,NextID)),
         final(Program,Final),
         simplify_max(Final,FinalS),
-        assert(cg_node(Program,FinalS,ID)).
+        assert(cg_node(ProgramName,Program,FinalS,ID)).
 
 % draw characteristic graph using dot
-cg_draw_graph :-
-        cgraph_file(CGraphFile),
+cg_draw_graph(ProgramName) :-
+        cgraph_file(CGraphFile,ProgramName),
         open(CGraphFile, write, Stream),
         write(Stream, 'digraph G {\n'),
-        cg_write_nodes(Stream),
-        cg_write_edges(Stream),
+        cg_write_nodes(Stream,ProgramName),
+        cg_write_edges(Stream,ProgramName),
         write(Stream, '}\n'),
         close(Stream).
 
-cg_write_nodes(Stream) :-
-        cg_node(_Program,_Final,ID),
+cg_write_nodes(Stream,ProgramName) :-
+        cg_node(ProgramName,_Program,_Final,ID),
         write(Stream, '\t'),
         write(Stream, ID),
         write(Stream, ';\n'),
         fail.
-cg_write_nodes(_Stream).
+cg_write_nodes(_Stream,_ProgramName).
 
-cg_write_edges(Stream) :-
-        cg_edge(ID,Action,NewID,_,_,_),
+cg_write_edges(Stream,ProgramName) :-
+        cg_edge(ProgramName,ID,Action,NewID,_,_,_),
         write(Stream, '\t'),
         write(Stream, ID),
         write(Stream, ' -> '),
@@ -131,8 +129,11 @@ cg_write_edges(Stream) :-
         % write(Stream, Condition),
         write(Stream, '\"];\n'),
         fail.
-cg_write_edges(_Stream).
+cg_write_edges(_ProgramName,_Stream).
 
-cgraph_file(File) :-
+cgraph_file(File,ProgramName) :-
         temp_dir(TempDir),
-        string_concat(TempDir, '/cgraph.dot', File).
+        atom_string(ProgramName,ProgramNameS),
+        string_concat('/', ProgramNameS, S),
+        string_concat(S, '_cgraph.dot', FileName),
+        string_concat(TempDir, FileName, File).
