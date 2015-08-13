74325794c6e68f1dab8723a5ae94f89a355e9d0c
Jens Claßen <classen@kbsg.rwth-aachen.de>
2015-08-13 16:55:53 +0000

    Refactored usable old stuff from cnf.pl into clausalform.pl.

---
+++ reasoning/clausalform.pl
@@ -0,0 +1,319 @@
+:- module(clausalform, []).
+
+:- use_module('../reasoning/fol').
+:- use_module('../lib/utils').
+
+:- dynamic(clause_n/2).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Clausal form conversions, CNF, DNF
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% fml2nnf(+Fml1,-Fml2,+FreeV,-Unis,-Exis,-AllVars,+SkolS1,-SkolS2,-Skol):
+% ** Fml2 is the negational normal form of Fml1 **
+%    -> FreeV are the free variables of Fml1
+%    -> Unis  are the universally quantified variables of Fml2
+%    -> Exis  are the existentially quantified variables of Fml2
+%    -> AllVars are all variables appearing in Fml2
+%       in the correct order (for the prenex form)
+%    -> SkolS1 is the next usable skolem function symbol *before*
+%       conversion (starting with 'skol1','skol2',...)
+%    -> SkolS2 is the next usable skolem function symbol *after*
+%       conversion
+%    -> Skol is the set of skolem terms introduced during conversion
+
+fml2nnf((F1<=>F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf((-F1+F2)*(F1+(F2)),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
+fml2nnf((F1=>F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf((-F1+F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
+fml2nnf((F1<=F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf((F1+(-F2)),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
+fml2nnf((F1*F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf(F1,Fml1,FreeV,Unis1,Exis1,AllVars1,SkolS1,SkolS3,Skol1),
+        fml2nnf(F2,Fml2,FreeV,Unis2,Exis2,AllVars2,SkolS3,SkolS2,Skol2),
+        append(Unis1,Unis2,Unis),
+        append(Exis1,Exis2,Exis),
+        append(AllVars1,AllVars2,AllVars),
+        append(Skol1,Skol2,Skol),
+        Fml=(Fml1*Fml2).
+fml2nnf((F1+F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf(F1,Fml1,FreeV,Unis1,Exis1,AllVars1,SkolS1,SkolS3,Skol1),
+        fml2nnf(F2,Fml2,FreeV,Unis2,Exis2,AllVars2,SkolS3,SkolS2,Skol2),
+        append(Unis1,Unis2,Unis),
+        append(Exis1,Exis2,Exis),
+        append(AllVars1,AllVars2,AllVars),
+        append(Skol1,Skol2,Skol),
+        Fml=(Fml1+Fml2).
+fml2nnf(all(Vars,F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        append(Vars,FreeV,FreeV1),
+        fml2nnf(F,Fml,FreeV1,Unis1,Exis,AllVars1,SkolS1,SkolS2,Skol),
+        append(Vars,Unis1,Unis),
+        append(Vars,AllVars1,AllVars).
+fml2nnf(some([Var|Vars],F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,[NewSymbol|Skol1]) :- !,
+        NewSymbol =.. [SkolS1|FreeV],
+        skol_next(SkolS1,SkolS3),
+        fml2nnf(some(Vars,F),Fml2,FreeV,Unis,Exis1,AllVars1,SkolS3,SkolS2,Skol1),
+        subv(Var,NewSymbol,Fml2,Fml),
+        Exis=[Var|Exis1],
+        AllVars=[Var|AllVars1].
+fml2nnf(some([],F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf(F,Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
+        
+fml2nnf(-(-F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf(F,Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
+fml2nnf(-(F1<=>F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf((F1+F2)*(-F1+(-F2)),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
+fml2nnf(-(F1=>F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf((F1*(-F2)),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
+fml2nnf(-(F1<=F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf((-F1*F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
+fml2nnf(-(F1*F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf((-F1+(-F2)),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
+fml2nnf(-(F1+F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf(((-F1)*(-F2)),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
+fml2nnf(-all(Vars,F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf(some(Vars,-F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
+fml2nnf(-some(Vars,F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
+        fml2nnf(all(Vars,-F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
+
+fml2nnf(Lit,Lit,_FreeV,[],[],[],SkolS,SkolS,[]).
+
+% skolem symbols are 'skol1','skol2','skol3',...
+skol_atom(skol).
+skol_first(SkolOne) :-
+        skol_atom(Skol),
+        atom_number(One,1),
+        atom_concat(Skol,One,SkolOne).
+skol_next(A1,A2) :-
+        skol_atom(Skol),
+        atom_concat(Skol,S1,A1),
+        atom_number(S1,N1),
+        N2 is N1+1,
+        atom_number(S2,N2),
+        atom_concat(Skol,S2,A2).
+
+% convert NNF into CNF by distributing disjunctions over conjunctions
+nnf2cnf((F1*F2)+F3,(Fml1*Fml2)) :- !,
+        nnf2cnf((F1|F3),Fml1),
+        nnf2cnf((F2|F3),Fml2).
+nnf2cnf(F1+(F2*F3),(Fml1*Fml2)) :- !,
+        nnf2cnf((F1|F2),Fml1),
+        nnf2cnf((F1|F3),Fml2).
+nnf2cnf((F1+F2),Fml) :- !,
+        nnf2cnf(F1,Fml1),
+        nnf2cnf(F2,Fml2),
+        ( (Fml1=(_A1*_B1);Fml2=(_A2*_B2)) -> nnf2cnf((Fml1+Fml2),Fml); Fml=(Fml1+Fml2)).
+nnf2cnf((F1*F2),(Fml1*Fml2)) :- !,
+        nnf2cnf(F1,Fml1),
+        nnf2cnf(F2,Fml2).
+nnf2cnf(Lit,Lit).
+
+% convert NNF into DNF by distributing conjunctions over disjunctions
+nnf2dnf((F1+F2)*F3,(Fml1+Fml2)) :- !,
+        nnf2dnf((F1*F3),Fml1),
+        nnf2dnf((F2*F3),Fml2).
+nnf2dnf(F1*(F2+F3),(Fml1+Fml2)) :- !,
+        nnf2dnf((F1*F2),Fml1),
+        nnf2dnf((F1*F3),Fml2).
+nnf2dnf((F1*F2),Fml) :- !,
+        nnf2dnf(F1,Fml1),
+        nnf2dnf(F2,Fml2),
+        ( (Fml1=(_A1+_B1);Fml2=(_A2+_B2)) -> nnf2dnf((Fml1*Fml2),Fml); Fml=(Fml1*Fml2)).
+nnf2dnf((F1+F2),(Fml1+Fml2)) :- !,
+        nnf2dnf(F1,Fml1),
+        nnf2dnf(F2,Fml2).
+nnf2dnf(Lit,Lit).
+
+% convert CNF formula in clause set
+cnf2clauses(F1*F2,Clauses) :- !,
+        cnf2clauses(F1,Clauses1),
+        cnf2clauses(F2,Clauses2),
+        append(Clauses1,Clauses2,Clauses).
+cnf2clauses(F1+F2,[Clause]) :- !,
+        cnf2clauses(F1,[Clause1]),
+        cnf2clauses(F2,[Clause2]),
+        append(Clause1,Clause2,Clause).
+cnf2clauses(true,[]).
+cnf2clauses(false,[[]]).
+cnf2clauses(Lit,[[Lit]]).
+
+% convert DNF formula in clause set
+dnf2clauses(F1+F2,Clauses) :- !,
+        dnf2clauses(F1,Clauses1),
+        dnf2clauses(F2,Clauses2),
+        append(Clauses1,Clauses2,Clauses).
+dnf2clauses(F1*F2,[Clause]) :- !,
+        dnf2clauses(F1,[Clause1]),
+        dnf2clauses(F2,[Clause2]),
+        append(Clause1,Clause2,Clause).
+dnf2clauses(false,[]).
+dnf2clauses(true,[[]]).
+dnf2clauses(Lit,[[Lit]]).
+
+% convert clause set to CNF formula
+clauses2cnf(Cla,Unis,Exis,Vars,Fml) :-
+        %make_fresh_variables(Unis,Skol,NewVars),
+        clauses2cnf(Cla,CNF),
+        attach_quantifiers(CNF,Unis,Exis,Vars,Fml2),
+        flatten_quantifiers(Fml2,Fml).
+
+clauses2cnf([Clause],Fml) :- !,
+        clause2disjunction(Clause,Fml).
+clauses2cnf([Clause|Clauses],(Fml1*Fml2)) :- !,
+        clause2disjunction(Clause,Fml1),
+        clauses2cnf(Clauses,Fml2).
+clauses2cnf([],true).
+
+clause2disjunction([Lit],Lit) :- !.
+clause2disjunction([Lit|Lits],(Lit+Fml)) :- !,
+        clause2disjunction(Lits,Fml).
+clause2disjunction([],false).
+
+% convert clause set to DNF formula
+clauses2dnf(Cla,Unis,Exis,Vars,Fml) :-
+        %make_fresh_variables(Unis,Skol,NewVars),
+        clauses2dnf(Cla,DNF),
+        attach_quantifiers(DNF,Unis,Exis,Vars,Fml2),
+        flatten_quantifiers(Fml2,Fml).
+
+clauses2dnf([Clause],Fml) :- !,
+        clause2conjunction(Clause,Fml).
+clauses2dnf([Clause|Clauses],(Fml1+Fml2)) :- !,
+        clause2conjunction(Clause,Fml1),
+        clauses2dnf(Clauses,Fml2).
+clauses2dnf([],false).
+
+clause2conjunction([Lit],Lit) :- !.
+clause2conjunction([Lit|Lits],(Lit*Fml)) :- !,
+        clause2conjunction(Lits,Fml).
+clause2conjunction([],true).
+
+% reattach quantifiers in prenex form
+attach_quantifiers(CNF,Unis,Exis,[X|Vars],Fml) :-
+        term_variables(CNF,TVars),
+        not(member2(X,TVars)),!, % if X not occurs, ignore
+        attach_quantifiers(Fml,CNF,Unis,Exis,Vars).
+attach_quantifiers(CNF,Unis,Exis,[X|Vars],Fml) :-
+        member2(X,Unis),!,
+        attach_quantifiers(Fml1,CNF,Unis,Exis,Vars),
+        Fml=all([X],Fml1).
+attach_quantifiers(CNF,Unis,Exis,[X|Vars],Fml) :-
+        member2(X,Exis),!,
+        attach_quantifiers(Fml1,CNF,Unis,Exis,Vars),
+        Fml=some([X],Fml1).
+attach_quantifiers(CNF,_Unis,_Exis,[],CNF).
+
+% flatten nested quantifiers using list notation
+flatten_quantifiers(all(Vars1,all(Vars2,Fml)),Result) :- !,
+        append(Vars1,Vars2,Vars),
+        flatten_quantifiers(all(Vars,Fml),Result).
+flatten_quantifiers(some(Vars1,some(Vars2,Fml)),Result) :- !,
+        append(Vars1,Vars2,Vars),
+        flatten_quantifiers(some(Vars,Fml),Result).
+flatten_quantifiers(some(Vars1, all(Vars2,Fml)),some(Vars1,Result)) :- !,
+        flatten_quantifiers(all(Vars2,Fml),Result).
+flatten_quantifiers(all(Vars1,some(Vars2,Fml)),all(Vars1,Result)) :- !,
+        flatten_quantifiers(some(Vars2,Fml),Result).
+flatten_quantifiers(Fml,Fml).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Prime implicate compiler
+% ------------------------
+% Adapted from Reiter's prime implicate compiler for open-world 
+% Golog, see chapter 10 of
+%     Raymond Reiter, Knowledge in Action: 
+%     Logical Foundations for Specifying and Implementing Dynamical
+%     Systems. MIT Press, 2001.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clauses2prime_implicates(Clauses,PrimeImplicates) :-
+        assertClauses(Clauses,0),
+        resolveAllClauses(0),
+        retractClauses(PrimeImplicates).
+
+assertClauses([],_N).
+assertClauses([Clause|Clauses],N) :-
+        assertClause(Clause,N),
+        assertClauses(Clauses,N).
+assertClause(Clause,_N) :- 
+        clause_tautology(Clause), !.
+assertClause(Clause,_N) :-
+        clause_subsumed(Clause), !.
+assertClause(Clause,N) :-
+        retractSubsumedClauses(Clause),
+        assert(clause_n(Clause,N)).
+
+retractSubsumedClauses(Clause) :-
+        clause_n(Clause2,N),
+        clause_subsumes(Clause,Clause2),
+        retract(clause_n(Clause2,N)),
+        fail.
+retractSubsumedClauses(_Clause).
+
+clause_subsumes(Clause1,Clause2) :-
+        subset2(Clause1,Clause2).
+
+clause_subsumed([Lit]) :- 
+        clause_n([Lit],_N).
+clause_subsumed([Lit1,Lit2|Lits]) :-
+        clause_n(Clause2,_N),
+        clause_subsumes(Clause2,[Lit1,Lit2|Lits]).
+
+clause_tautology([true]).
+clause_tautology([Lit1,Lit2|Lits]) :-
+        member(L,[Lit1,Lit2|Lits]),
+        L \= -_,
+        member(-L,[Lit1,Lit2|Lits]).
+
+resolveAllClauses(N) :-
+        resolveAllClausesUnit(N),
+        resolveAllClausesNonUnit(N).
+resolveAllClauses(N) :-
+        N1 is N+1,
+        clause_n(_Clause,N1),
+        resolveAllClauses(N1).
+resolveAllClauses(_N).
+
+resolveAllClausesUnit(N) :- 
+        N1 is N+1, 
+        (clause_n([Lit],N), 
+         clause_n(Clause,_J);
+         clause_n([Lit],J),
+         J < N, 
+         clause_n(Clause,N)),
+        unitResolvent([Lit],Clause,Result),
+        retractSubsumedClauses(Result),
+        assert(clause_n(Result,N1)), 
+        fail.
+resolveAllClausesUnit(_N).
+
+unitResolvent([Lit],Clause,Result) :- 
+        complement(Lit,CLit), 
+        member(CLit,Clause),
+        setminus2(Clause,[CLit],Result).
+
+resolveAllClausesNonUnit(N) :- 
+        N1 is N+1,
+        clause_n([Lit1,Lit2|Lits1],N),
+        clause_n([Lit3,Lit4|Lits2],_J),
+        resolvent([Lit1,Lit2|Lits1],[Lit3,Lit4|Lits2],Clause),
+        assertClause(Clause,N1),
+        fail.
+
+resolvent(Clause1,Clause2,Clause) :-
+        member(Lit1,Clause1),
+        complement(Lit1,Lit2),
+        member(Lit2,Clause2),
+        setminus2(Clause1,[Lit1],Clause3),
+        setminus2(Clause2,[Lit2],Clause4), 
+        union2(Clause3,Clause4,Clause).
+
+complement(A,-A) :- 
+        A \= (-_).
+complement(-A,A).
+
+retractClauses([Clause|Clauses]) :-
+        retract(clause_n(Clause,_N)), !,
+        retractClauses(Clauses).
+retractClauses([]).
+
--- reasoning/cnf.pl
+++ reasoning/cnf.pl
@@ -1,4 +1,4 @@
-:- module(clausalform).
+%- module(clausalform).
 
 :- use_module(tptp).
 :- use_module(utils).
@@ -16,22 +16,10 @@
 
 :- export(elim/3).
 
-:- export equivalent/2.
-:- export equivalent/3.
-:- export entails/3.
-
 %:- get_flag(output_options,Old),
 %   set_flag(output_options,[variables(full)|Old]).
 
 :- dynamic(clause_dnf/4).
-
-:- dynamic equivalent_cached/3.
-:- dynamic nonequiv_cached/3.
-
-uno(e).
-uno(p).
-functional_fluent(q1).
-functional_fluent(q2).
 
 % todo: deal with unused variables!
 
@@ -43,170 +31,12 @@ clausalform_cnf(Fml,Cla,Unis,Exis,Vars,Skol) :-
         cf_cnf_clauses(Fml2,Cla1),
         cf_cnf_clauses_simplify(Cla1,Unis,Exis,Vars,Cla).
 
-% cf_nnf(+Fml1,-Fml2,-Fml3,+FreeV,-Unis,+SkolS1,-SkolS2,-Skol):
-% ** Fml2 is the negational normal form of Fml1 **
-%    -> FreeV are the free variables of Fml1
-%    -> Unis  are the universally quantified variables of Fml2
-%    -> Exis  are the existentially quantified variables of Fml2
-%    -> AllVars are all variables appearing in Fml2
-%       in the correct order (for the prenex form)
-%    -> SkolS1 is the next usable skolem function symbol *before*
-%       conversion (starting with 'skol1','skol2',...)
-%    -> SkolS2 is the next usable skolem function symbol *after*
-%       conversion
-%    -> Skol is the set of skolem terms introduced during conversion
-
-cf_nnf((F1<=>F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf((~F1|F2)&(F1|~F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
-cf_nnf((F1=>F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf((~F1|F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
-cf_nnf((F1<=F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf((F1|~F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
-cf_nnf((F1&F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf(F1,Fml1,FreeV,Unis1,Exis1,AllVars1,SkolS1,SkolS3,Skol1),
-        cf_nnf(F2,Fml2,FreeV,Unis2,Exis2,AllVars2,SkolS3,SkolS2,Skol2),
-        append(Unis1,Unis2,Unis),
-        append(Exis1,Exis2,Exis),
-        append(AllVars1,AllVars2,AllVars),
-        append(Skol1,Skol2,Skol),
-        Fml=(Fml1&Fml2).
-cf_nnf((F1|F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf(F1,Fml1,FreeV,Unis1,Exis1,AllVars1,SkolS1,SkolS3,Skol1),
-        cf_nnf(F2,Fml2,FreeV,Unis2,Exis2,AllVars2,SkolS3,SkolS2,Skol2),
-        append(Unis1,Unis2,Unis),
-        append(Exis1,Exis2,Exis),
-        append(AllVars1,AllVars2,AllVars),
-        append(Skol1,Skol2,Skol),
-        Fml=(Fml1|Fml2).
-cf_nnf(!Vars:F,Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        append(Vars,FreeV,FreeV1),
-        cf_nnf(F,Fml,FreeV1,Unis1,Exis,AllVars1,SkolS1,SkolS2,Skol),
-        append(Vars,Unis1,Unis),
-        append(Vars,AllVars1,AllVars).
-cf_nnf(?[Var|Vars]:F,Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,[NewSymbol|Skol1]) :- !,
-        NewSymbol =.. [SkolS1|FreeV],
-        %Var = NewSymbol,
-        skol_next(SkolS1,SkolS3),
-        cf_nnf(?Vars:F,Fml,FreeV,Unis,Exis1,AllVars1,SkolS3,SkolS2,Skol1),
-        Exis=[Var|Exis1],
-        AllVars=[Var|AllVars1].
-cf_nnf(?[]:F,Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf(F,Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
-        
-cf_nnf(~(~F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf(F,Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
-cf_nnf(~(F1<=>F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf((F1|F2)&(~F1|~F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
-cf_nnf(~(F1=>F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf((F1&(~F2)),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
-cf_nnf(~(F1<=F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf((~F1&F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
-cf_nnf(~(F1&F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf((~F1|~F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
-cf_nnf(~(F1|F2),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf(((~F1)&(~F2)),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
-cf_nnf(~(!Vars:F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf(?Vars:(~F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
-cf_nnf(~(?Vars:F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol) :- !,
-        cf_nnf(!Vars:(~F),Fml,FreeV,Unis,Exis,AllVars,SkolS1,SkolS2,Skol).
-
-cf_nnf(Lit,Lit,_FreeV,[],[],[],SkolS,SkolS,[]).
-
-% skolem symbols are 'skol1','skol2','skol3',...
-skol_next(A1,A2) :-
-        atom_string(A1,S1),
-        append_strings("skol",N1,S1),
-        number_string(I1,N1),
-        I2 is I1+1,
-        number_string(I2,N2),
-        append_strings("skol",N2,S2),
-        atom_string(A2,S2).
-
-% convert NNF into CNF by distributing disjunctions over conjunctions
-cf_cnf((F1&F2)|F3,(Fml1&Fml2)) :- !,
-        cf_cnf((F1|F3),Fml1),
-        cf_cnf((F2|F3),Fml2).
-cf_cnf(F1|(F2&F3),(Fml1&Fml2)) :- !,
-        cf_cnf((F1|F2),Fml1),
-        cf_cnf((F1|F3),Fml2).
-cf_cnf((F1|F2),Fml) :- !,
-        cf_cnf(F1,Fml1),
-        cf_cnf(F2,Fml2),
-        ( (Fml1=(_A1&_B1);Fml2=(_A2&_B2)) -> cf_cnf((Fml1|Fml2),Fml); Fml=(Fml1|Fml2)).
-cf_cnf((F1&F2),(Fml1&Fml2)) :- !,
-        cf_cnf(F1,Fml1),
-        cf_cnf(F2,Fml2).
-cf_cnf(Lit,Lit).
-
-% convert CNF formula in clause set
-cf_cnf_clauses(F1&F2,Clauses) :- !,
-        cf_cnf_clauses(F1,Clauses1),
-        cf_cnf_clauses(F2,Clauses2),
-        append(Clauses1,Clauses2,Clauses).
-cf_cnf_clauses(F1|F2,[Clause]) :- !,
-        cf_cnf_clauses(F1,[Clause1]),
-        cf_cnf_clauses(F2,[Clause2]),
-        append(Clause1,Clause2,Clause).
-cf_cnf_clauses($true,[]).
-cf_cnf_clauses($false,[[]]).
-cf_cnf_clauses(Lit,[[Lit]]).
-
-
 cf_cnf_clauses_simplify(Cla1,_U,_E,_V,Cla1).% :-
         %remove true
         %remove false
         %remove subsumed variants (incl. duplicates, variants)
         %unit propagation?
         %exists x.x=t
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-clauses2fml_cnf(Fml,Cla,Unis,Exis,Vars) :-
-        %make_fresh_variables(Unis,Skol,NewVars),
-        clauses2cnf(Cla,CNF),
-        attach_quantifiers(Fml1,CNF,Unis,Exis,Vars),
-        flatten_quantifiers(Fml1,Fml).
-
-clauses2cnf([Clause],Fml) :- !,
-        clause2disjunction(Clause,Fml).
-clauses2cnf([Clause|Clauses],(Fml1&Fml2)) :- !,
-        clause2disjunction(Clause,Fml1),
-        clauses2cnf(Clauses,Fml2).
-clauses2cnf([],$true).
-
-clause2disjunction([Lit],Lit) :- !.
-clause2disjunction([Lit|Lits],(Lit|Fml)) :- !,
-        clause2disjunction(Lits,Fml).
-clause2disjunction([],$false).
-
-% if X does not appear in CNF => ignore
-attach_quantifiers(Fml,CNF,Unis,Exis,[X|Vars]) :-
-        term_variables(CNF,TVars),
-        not(member2(X,TVars)),!,
-        attach_quantifiers(Fml,CNF,Unis,Exis,Vars).
-
-attach_quantifiers(Fml,CNF,Unis,Exis,[X|Vars]) :-
-        member2(X,Unis),!,
-        attach_quantifiers(Fml1,CNF,Unis,Exis,Vars),
-        Fml=(![X]:Fml1).
-attach_quantifiers(Fml,CNF,Unis,Exis,[X|Vars]) :-
-        member2(X,Exis),!,
-        attach_quantifiers(Fml1,CNF,Unis,Exis,Vars),
-        Fml=(?[X]:Fml1).
-attach_quantifiers(CNF,CNF,_Unis,_Exis,[]).
-
-flatten_quantifiers(!Vars1:(!Vars2:Fml),Result) :- !,
-        append(Vars1,Vars2,Vars),
-        flatten_quantifiers(!Vars:Fml,Result).
-flatten_quantifiers(?Vars1:(?Vars2:Fml),Result) :- !,
-        append(Vars1,Vars2,Vars),
-        flatten_quantifiers(?Vars:Fml,Result).
-flatten_quantifiers(?Vars1:(!Vars2:Fml),?Vars1:Result) :- !,
-        flatten_quantifiers(!Vars2:Fml,Result).
-flatten_quantifiers(!Vars1:(?Vars2:Fml),!Vars1:Result) :- !,
-        flatten_quantifiers(?Vars2:Fml,Result).
-flatten_quantifiers(Fml,Fml).
 
 %% % generate new variables for deskolemization
 %% make_fresh_variables(Unis,[Skol|Skols],[X|NewVars]) :-
@@ -227,36 +57,6 @@ clausalform_dnf(Fml,Cla,Unis,Exis,Vars,Skol) :-
         cf_dnf(Fml1,Fml2),
         cf_dnf_clauses(Fml2,Cla1),
         cf_dnf_clauses_simplify(Cla1,Unis,Exis,Vars,Cla).
-
-% convert NNF into DNF by distributing conjunctions over disjunctions
-cf_dnf((F1|F2)&F3,(Fml1|Fml2)) :- !,
-        cf_dnf((F1&F3),Fml1),
-        cf_dnf((F2&F3),Fml2).
-cf_dnf(F1&(F2|F3),(Fml1|Fml2)) :- !,
-        cf_dnf((F1&F2),Fml1),
-        cf_dnf((F1&F3),Fml2).
-cf_dnf((F1&F2),Fml) :- !,
-        cf_dnf(F1,Fml1),
-        cf_dnf(F2,Fml2),
-        ( (Fml1=(_A1|_B1);Fml2=(_A2|_B2)) -> cf_dnf((Fml1&Fml2),Fml); Fml=(Fml1&Fml2)).
-cf_dnf((F1|F2),(Fml1|Fml2)) :- !,
-        cf_dnf(F1,Fml1),
-        cf_dnf(F2,Fml2).
-cf_dnf(Lit,Lit).
-
-% convert DNF formula in clause set
-cf_dnf_clauses(F1|F2,Clauses) :- !,
-        cf_dnf_clauses(F1,Clauses1),
-        cf_dnf_clauses(F2,Clauses2),
-        append(Clauses1,Clauses2,Clauses).
-cf_dnf_clauses(F1&F2,[Clause]) :- !,
-        cf_dnf_clauses(F1,[Clause1]),
-        cf_dnf_clauses(F2,[Clause2]),
-        append(Clause1,Clause2,Clause).
-cf_dnf_clauses($false,[]).
-cf_dnf_clauses($true,[[]]).
-cf_dnf_clauses(Lit,[[Lit]]).
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -619,12 +419,6 @@ variant_subsumed3([Lit|Lits],[_Lit2|Lits2],Lits3) :-
 variant_subsumed3([],Lits2,Lits2).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 cf_dnf_simplify_resolution(Clauses1,Clauses2,U,E,V) :-
         assert_clauses_dnf(Clauses1,U,E,V),
@@ -701,81 +495,11 @@ elim_clauses([],Cla,Cla,_U,_E,_V).
 %%         clauses2fml(Phi2,Cla2,Unis,Exis,Vars).
 %% elim([],Phi,Phi).
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-clauses2fml_dnf(Fml,Cla,Unis,Exis,Vars) :-
-        %make_fresh_variables(Unis,Skol,NewVars),
-        clauses2dnf(Cla,DNF),
-        attach_quantifiers(Fml1,DNF,Unis,Exis,Vars),
-        flatten_quantifiers(Fml1,Fml).
-
-clauses2dnf([Clause],Fml) :- !,
-        clause2conjunction(Clause,Fml).
-clauses2dnf([Clause|Clauses],(Fml1|Fml2)) :- !,
-        clause2conjunction(Clause,Fml1),
-        clauses2dnf(Clauses,Fml2).
-clauses2dnf([],$false).
-
-clause2conjunction([Lit],Lit) :- !.
-clause2conjunction([Lit|Lits],(Lit&Fml)) :- !,
-        clause2conjunction(Lits,Fml).
-clause2conjunction([],$true).
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-        
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-equivalent(Fml1,Fml2) :- equivalent(Fml1,Fml2,[]).
-
-equivalent(Fml1,Fml2,Axioms) :- equivalent_cached(Fml1,Fml2,Axioms), !.
-equivalent(Fml1,Fml2,Axioms) :- nonequiv_cached(Fml1,Fml2,Axioms), !, fail.
-equivalent(Fml1,Fml2,Axioms) :- determine_equivalence(Fml1,Fml2,Axioms,Truth),
-        (Truth = ($true) -> asserta(equivalent_cached(Fml1,Fml2,Axioms));
-                            (asserta(nonequiv_cached(Fml1,Fml2,Axioms)),fail)).
-
-determine_equivalence(Fml1,Fml2,_Axioms,Truth) :- variant(Fml1,Fml2), !, Truth=($true).
-
-determine_equivalence(Fml1,Fml2,Axioms,($true)) :-
-        entails(Fml1,Fml2,Axioms),
-        entails(Fml2,Fml1,Axioms), !.
-
-%% determine_equivalence(Fml1,Fml2,Axioms,($true)) :-
-%%         write_problem_file(Fml1,Fml2,Axioms,'conjecture.tptp'),
-%%         exec(["./prove", "conjecture.tptp", "output.txt"],[]),
-%%         sh("rm conjecture.tptp"),
-%%         open('output.txt',read,result),
-%%         read_string(result,end_of_line,_L2,SecondLine),
-%%         close(result),
-%%         sh("rm output.txt"),
-%%         SecondLine =="% SZS status Theorem", !.
-
 %% entails(Fml1,Fml2,Axioms) :-
 %%         term_variables((Fml1,Fml2),[]),
 %%         clausalform_dnf(Fml1,Cla1,[],[],[],[]),
 %%         clausalform_dnf(Fml2,Cla2,[],[],[],[]),!,
 %%         clausal_subsumption_dnf(Cla1,Cla2).
-        
-        
-        
-entails(Fml1,Fml2,Axioms) :-
-        write_problem_file(Fml2,[Fml1|Axioms],'conjecture.tptp'),
-        exec(["./E/bin/eprover", "--tptp3-in", "-m", "2048", "-s",
-              "conjecture.tptp", "-o", "output.tptp"],[]),
-        %sh("rm conjecture.tptp"),
-        sh("mv conjecture.tptp conjecture_old.tptp"),
-        open('output.tptp',read,result),
-        read_string(result,end_of_line,_L1,_FirstLine),
-        read_string(result,end_of_line,_L2,SecondLine),
-        close(result),
-        sh("rm output.tptp"),
-        SecondLine == "# Proof found!".
 
 clausal_subsumption_dnf(Clauses,[Clause2|Clauses2]) :-
         member(Clause,Clauses),
@@ -785,67 +509,3 @@ clausal_subsumption_dnf(Clauses,[Clause2]) :-
         member(Clause,Clauses),
         subset2(Clause2,Clause).
 
-
-write_problem_file(Fml,Axioms,FileName) :-
-        open(FileName,write,conjecture),
-        formula_string(Fml,FormulaS),
-        axioms_string(Axioms,AxiomsS),
-        concat_string(["fof(conj,conjecture,",FormulaS,")."],EquiSD),
-        write(conjecture,AxiomsS),
-        writeln(conjecture,EquiSD),
-        close(conjecture).
-        
-
-determine_equivalence(_Fml1,_Fml2,($false)).
-
-axioms_string(Axioms,String) :-
-        axioms_string(Axioms,String,1).
-
-axioms_string([],"",_).
-axioms_string([A|Axioms],S,I) :-
-        I1 is I + 1,
-        axioms_string(Axioms,AxiomsS,I1),
-        formula_string(A,AS),
-        integer_atom(I,IA),
-        atom_string(IA,IS),
-        concat_string(["fof(axiom", IS, ",axiom,",AS,").\n", AxiomsS],S).
-
-formula_string(~Fml,S) :- !,
-        formula_string(Fml,FS),
-        concat_string(["~(",FS,")"],S).
-formula_string(F1|F2,S) :- !,
-        formula_string(F1,FS1),
-        formula_string(F2,FS2),
-        concat_string(["(",FS1,")|(",FS2,")"],S).
-formula_string(F1&F2,S) :- !,
-        formula_string(F1,FS1),
-        formula_string(F2,FS2),
-        concat_string(["(",FS1,")&(",FS2,")"],S).
-formula_string(F1=>F2,S) :- !,
-        formula_string(F1,FS1),
-        formula_string(F2,FS2),
-        concat_string(["(",FS1,")=>(",FS2,")"],S).
-formula_string(F1<=>F2,S) :- !,
-        formula_string(F1,FS1),
-        formula_string(F2,FS2),
-        concat_string(["(",FS1,")<=>(",FS2,")"],S).
-formula_string(F1<=F2,S) :- !,
-        formula_string(F1,FS1),
-        formula_string(F2,FS2),
-        concat_string(["(",FS1,")<=(",FS2,")"],S).
-formula_string(!Vars:Fml,S) :- !,
-        term_string(Vars,VarsS),
-        formula_string(Fml,FS),
-        concat_string(["!",VarsS,":(",FS,")"],S).
-formula_string(?Vars:Fml,S) :- !,
-        term_string(Vars,VarsS),
-        formula_string(Fml,FS),
-        concat_string(["?",VarsS,":(",FS,")"],S).
-formula_string(X=Y,S) :- !,
-        term_string(X,XS),
-        term_string(Y,YS),
-        concat_string(["(",XS," = ",YS,")"],S).
-formula_string($true,"$true") :- !.
-formula_string($false,"$false") :- !.
-formula_string(Atom,S) :-
-        term_string(Atom,S).
