99e92793024317a723612d2e032b0abe8cf9eb4c
Jens Claßen <classen@kbsg.rwth-aachen.de>
2015-08-20 12:33:42 +0000

    Bugxfix: wrong dependency simplifications.

--- reasoning/fobdd.pl
+++ reasoning/fobdd.pl
@@ -474,17 +474,17 @@ simplify_deps(Fml1*Fml2,Vars,false) :-
 simplify_deps(Fml1*Fml2,Vars,false) :-
         implies(Fml2,-Fml1,Vars), !.
 simplify_deps(Fml1*(Fml2+Fml3),Vars,Fml) :-
-        implies(Fml1*Fml2,Fml3,Vars), !,
-        simplify_deps(Fml3,Vars,Fml).
+        implies(Fml1*Fml2,Fml1*Fml3,Vars), !,
+        simplify_deps(Fml1*Fml3,Vars,Fml).
 simplify_deps(Fml1*(Fml2+Fml3),Vars,Fml) :-
-        implies(Fml1*Fml3,Fml2,Vars), !,
-        simplify_deps(Fml2,Vars,Fml).
+        implies(Fml1*Fml3,Fml1*Fml2,Vars), !,
+        simplify_deps(Fml1*Fml2,Vars,Fml).
 simplify_deps((Fml1+Fml2)*Fml3,Vars,Fml) :-
-        implies(Fml1*Fml3,Fml2,Vars), !,
-        simplify_deps(Fml2,Vars,Fml).
+        implies(Fml1*Fml3,Fml2*Fml3,Vars), !,
+        simplify_deps(Fml2*Fml3,Vars,Fml).
 simplify_deps((Fml1+Fml2)*Fml3,Vars,Fml) :-
-        implies(Fml2*Fml3,Fml1,Vars), !,
-        simplify_deps(Fml1,Vars,Fml).
+        implies(Fml2*Fml3,Fml1*Fml3,Vars), !,
+        simplify_deps(Fml1*Fml3,Vars,Fml).
 simplify_deps(Fml1*(Fml2+Fml3),Vars,Fml) :-
         implies(Fml3,-Fml1,Vars), !,
         simplify_deps(Fml1*Fml2,Vars,Fml).
