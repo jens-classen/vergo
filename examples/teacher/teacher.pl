0de50c54801696d23fb87c0331fab5af30e6484d
Jens Claßen <classen@kbsg.rwth-aachen.de>
2015-08-26 16:15:28 +0000

    Added teacher example files.

---
+++ examples/teacher/teacher.pl
@@ -0,0 +1,8 @@
+
+initially(teach(tom, sam)).
+initially(teach(tina, sue) * (teach(tom, sue) + teach(ted, sue))).
+initially(some(X,teach(X, sara))).
+initially(all(X,(teach(X, sandy) <=> (X=ted)))).
+initially(all([X,Y],(teach(X, Y) => ((Y=sam) + (Y=sue) + (Y=sara) + (Y= sandy))))).
+
+stdname(X) :- member(X, [tom,tina,ted,sam,sue,sara,sandy]).
