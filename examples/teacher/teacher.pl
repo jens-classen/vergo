4f7937cae314b11573b1efee71d4da427cb1611c
Jens Claßen <classen@kbsg.rwth-aachen.de>
2016-06-22 13:13:54 +0000

    Added test queries to teacher example.

--- examples/teacher/teacher.pl
+++ examples/teacher/teacher.pl
@@ -8,3 +8,49 @@ initially(all(X,(teach(X, sandy) <=> (X=ted)))).
 initially(all([X,Y],(teach(X, Y) => ((Y=sam) + (Y=sue) + (Y=sara) + (Y= sandy))))).
 
 stdname(X) :- member(X, [tom,tina,ted,sam,sue,sara,sandy]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Testing
+% -------
+% Reconstruction of full Example from Section 5.7.1 of
+% 
+% Hector J. Levesque and Gerhard Lakemeyer: The Logic of Knowledge
+% Bases. MIT Press, 2001.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% TODO: TELL examples
+
+/**
+  * query(+Number,+Formula,+ExpectResultForAsk4)
+  **/
+query( 1,teach(tom,sam),true).
+query( 2,teach(tom,sandy),false).
+query( 3,teach(tom,sue),unknown).
+query( 4,know(teach(tom,sue)),false).
+query( 5,some(X,teach(X,sara)),true).
+query( 6,some(X,know(teach(X,sara))),false).
+query( 7,some(X,know(teach(X,sue))),true).
+query( 8,some(X,teach(X,sue) * -know(teach(X,sue))),true).
+query( 9,some(X,teach(X,sandy) * -know(teach(X,sandy))),false).
+query(10,some(X,teach(X,sam) * -know(teach(X,sam))),unknown).
+query(11,some(Y,know(all(X,teach(X,Y) => know(teach(X,Y))))),true).
+query(12,some(Y,-(Y=sam) * -kwhether(all(X,teach(X,Y)=>know(teach(X,Y))))),false).
+query(13,some(Y,know(some(X,teach(X,Y) * some(Z,-(Y=Z)*know(teach(X,Z)))))),true).
+
+def(kwhether(F),know(F)+know(-F)).
+
+test_all_queries :-
+        test_query(N),
+        fail.
+test_all_queries.
+
+test_query(Number) :-
+        query(Number,Formula,ExpectedResult),
+        ask4(Formula,Result),
+        report_result(Number,Formula,Result,ExpectedResult).
+
+report_result(Number,Formula,Result,ExResult) :-
+        report_message(['Query          : ', Number, '\n',
+                        'Formula        : ', Formula, '\n',
+                        'Result         : ', Result, '\n',
+                        'Expected Result: ', ExResult, '\n']).
