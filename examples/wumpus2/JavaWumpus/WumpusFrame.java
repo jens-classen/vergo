546d23a01e75df87cef3de5a63544ea3a0491bc6
Jens Claßen <classen@kbsg.rwth-aachen.de>
2017-08-02 15:42:40 +0200

    First draft for Java Wumpus applet.

---
+++ examples/wumpus2/JavaWumpus/WumpusFrame.java
@@ -0,0 +1,14 @@
+public class WumpusFrame extends javax.swing.JFrame {
+
+    public static void main(String[] args) {
+        
+        WumpusFrame wumpusFrame = new WumpusFrame();
+        wumpusFrame.setTitle("Wumpus");
+        wumpusFrame.setSize(1000, 620);
+	wumpusFrame.setResizable(false);
+	wumpusFrame.setLocation(50, 50);
+	wumpusFrame.setVisible(true);
+        
+    }
+    
+}
