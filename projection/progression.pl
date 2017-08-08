e52dba66301bdab6233cb15e742e8d944004486e
Jens Claßen <classen@kbsg.rwth-aachen.de>
2017-08-08 17:50:13 +0200

    Outcommented incomplete local-effect progression.

--- projection/progression.pl
+++ projection/progression.pl
@@ -92,14 +92,14 @@ adl_holds(all(V,F)) :- !,
 % Progression for local-effect theories
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-progress(Action,local_effect) :-
-        ground(Action), !,
-        findall(Fluent,
-                (causes_false(Action,Fluent,_Cond1);
-                 causes_true(Action,Fluent,_Cond2)),
-                CharacteristicSet).
-        % generate all combinations / truth assignment
-        % generate all instantiated SSAs
-        % unify each with initial theory
-        % apply regression from verification_abstraction to each
-        % disjoin them all = result
+% progress(Action,local_effect) :-
+%         ground(Action), !,
+%         findall(Fluent,
+%                 (causes_false(Action,Fluent,_Cond1);
+%                  causes_true(Action,Fluent,_Cond2)),
+%                 CharacteristicSet).
+%         % generate all combinations / truth assignment
+%         % generate all instantiated SSAs
+%         % unify each with initial theory
+%         % apply regression from verification_abstraction to each
+%         % disjoin them all = result
