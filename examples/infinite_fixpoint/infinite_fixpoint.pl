0008f70158abdb6ee2719e11d36f4dfdf09bed35
Jens Claßen <classen@kbsg.rwth-aachen.de>
2018-08-22 16:33:13 +0200

    Example of a program and temporal property with infinite fixpoint.

---
+++ examples/infinite_fixpoint/infinite_fixpoint.pl
@@ -0,0 +1,36 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Simple Example that generates an infinite fixpoint, i.e.
+% the fixpoint verification method will not terminate (the
+% solution here would have to be a second-order formula that
+% expresses the property that initially, there are infinitely
+% many instances of the f fluent).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+:- discontiguous causes_true/3.
+:- discontiguous causes_false/3.
+:- discontiguous rel_fluent/1.
+
+%initially(...).
+
+prim_action(a(_)).
+prim_action(b(_)).
+
+rel_fluent(f(_)).
+
+poss(a(_),true).
+poss(b(_),true).
+
+causes_true(b(X),f(X),true).
+causes_false(a(X),f(X),true).
+
+%include_preconditions. % no precondition extension
+
+program(main,
+        [while(some(X,f(X)),
+               pick(X,[test(f(X)),
+                       a(X)])),
+         b('#c')]).
+
+property(prop1,
+         main,
+         somepath(always(-f('#c')))).
