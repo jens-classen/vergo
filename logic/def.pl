ef0bdf6cca1081a3738e354d3d23009b9860ec53
Jens Classen <jens_classen@sfu.ca>
2019-11-15 10:33:02 -0800

    Moved macro expansion code to separte module.

---
+++ logic/def.pl
@@ -0,0 +1,26 @@
+:- module(def, [expand_defs/2]).
+
+:- use_module('l').
+:- use_module('../lib/utils').
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Macro expand defined formulas
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+expand_defs(Fml1,Fml2) :-
+        nonvar(Fml1),
+        def(Fml1,Fml3), !,
+        expand_defs(Fml3,Fml2).
+expand_defs(Fml1,Fml2) :-
+        nonvar(Fml1),
+        not(atomic(Fml1)), !,
+        Fml1 =.. [Fml3|Fml4],
+        expand_defs(Fml3,Fml5),
+        expand_defs_list(Fml4,Fml6),
+        Fml2 =.. [Fml5|Fml6].
+expand_defs(Fml,Fml) :- !.
+
+expand_defs_list([],[]) :- !.
+expand_defs_list([X|Xs],[Y|Ys]) :- !,
+        expand_defs(X,Y),
+        expand_defs_list(Xs,Ys).
--- logic/l_kb.pl
+++ logic/l_kb.pl
@@ -2,6 +2,7 @@
                  extend_initial_kb_by/1,
                  print_kb/0]).
 
+:- use_module('def').
 :- use_module('l').
 :- use_module('../lib/utils').
 
@@ -37,25 +38,3 @@ extend_initial_kb_by(Fml) :-
         entails_initially(Fml,true), !.
 extend_initial_kb_by(Fml) :- !,
         assert(initially(Fml)).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Helper predicates
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-expand_defs(Fml1,Fml2) :-
-        nonvar(Fml1),
-        def(Fml1,Fml3), !,
-        expand_defs(Fml3,Fml2).
-expand_defs(Fml1,Fml2) :-
-        nonvar(Fml1),
-        not(atomic(Fml1)), !,
-        Fml1 =.. [Fml3|Fml4],
-        expand_defs(Fml3,Fml5),
-        expand_defs_list(Fml4,Fml6),
-        Fml2 =.. [Fml5|Fml6].
-expand_defs(Fml,Fml) :- !.
-
-expand_defs_list([],[]) :- !.
-expand_defs_list([X|Xs],[Y|Ys]) :- !,
-        expand_defs(X,Y),
-        expand_defs_list(Xs,Ys).
