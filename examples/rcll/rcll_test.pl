a5ad12f90334c609b35684c61e7254e405813e57
Jens Classen <jens_classen@sfu.ca>
2020-07-26 17:57:12 -0700

    Updated RCLL example with new version of PDDL parser.

---
+++ examples/rcll/rcll_test.pl
@@ -0,0 +1,7 @@
+:- use_module('../../tools/pddl_parser').
+
+% Parse RCLL PDDL domain description.
+% So far, duration constraints are ignored.
+test :-
+        parse_pddl_domain(file('rcll_domain_production_durations.pddl'),
+                          file('rcll.pl'),_S).
