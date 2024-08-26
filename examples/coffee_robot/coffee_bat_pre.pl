568baf7104cd00ff3225a46820e6f4549678464b
Jens Classen <classen@ruc.dk>
2024-08-26 15:15:39 +0200

    Rearranged predicate version of coffee robot BAT.

--- examples/coffee_robot/coffee_bat_pre.pl
+++ examples/coffee_robot/coffee_bat_pre.pl
@@ -12,44 +12,42 @@
 :- discontiguous rel_fluent/1.
 :- discontiguous def/2.
 
-initially(-holdingCoffee).
-initially(all(X,-hasCoffee(X))).
-initially(empty).
-
+rel_fluent(hasCoffee(_)).
 rel_fluent(holdingCoffee).
 rel_fluent(queue(_,_)).
-rel_fluent(hasCoffee(_)).
 
-poss(wait,true).
+def(first(P),
+    some(P2,queue(P,P2))).
+def(last(P),
+    some(P2,queue(P2,P))).
+def(empty,
+    queue('#e','#e')).
+def(enqueue(Xo,Yo,P,X,Y),
+    ((Xo='#e')*(Yo='#e')*(X=P)*(Y='#e'))
+    +some(X1,(-(X1='#e'))*(Xo=X1)*(Yo='#e')*(X=X1)*(Y=P))).
+def(dequeue(Xo,Yo,P,X,Y),
+    some(X2,(Xo=P)*(Yo=X2)*(X=X2)*(Y='#e'))).
+
+initially(all(X,-hasCoffee(X))).
+initially(-holdingCoffee).
+initially(empty).
+
 poss(requestCoffee(P),-(P='#e')*last('#e')).
 poss(selectRequest(P),-(P='#e')*first(P)).
 poss(pickupCoffee,-holdingCoffee).
 poss(bringCoffee(_P),holdingCoffee).
-
-causes_true(pickupCoffee,holdingCoffee,true).
-causes_false(bringCoffee(_),holdingCoffee,true).
+poss(wait,true).
 
 causes_true(requestCoffee(P),queue(X,Y),some([Xo,Yo],queue(Xo,Yo)*enqueue(Xo,Yo,P,X,Y))).
 causes_false(requestCoffee(P),queue(Xo,Yo),some([X,Y],queue(Xo,Yo)*enqueue(Xo,Yo,P,X,Y))).
 causes_true(selectRequest(P),queue(X,Y),some([Xo,Yo],queue(Xo,Yo)*dequeue(Xo,Yo,P,X,Y))).
 causes_false(selectRequest(P),queue(Xo,Yo),some([X,Y],queue(Xo,Yo)*dequeue(Xo,Yo,P,X,Y))).
 
+causes_true(pickupCoffee,holdingCoffee,true).
+causes_false(bringCoffee(_),holdingCoffee,true).
+
 causes_true(bringCoffee(P),hasCoffee(P),true).
 causes_false(requestCoffee(P),hasCoffee(P),true).
-
-def(first(P),
-    some(P2,queue(P,P2))).
-def(empty,    
-    queue('#e','#e')).
-def(last(P),
-    some(P2,queue(P2,P))).
-def(full,
-    some([P1,P2],(-(P1='#e'))*(-(P2='#e'))*queue(P1,P2))).
-def(enqueue(Xo,Yo,P,X,Y),
-    ((Xo='#e')*(Yo='#e')*(X=P)*(Y='#e'))
-    +some(X1,(-(X1='#e'))*(Xo=X1)*(Yo='#e')*(X=X1)*(Y=P))).
-def(dequeue(Xo,Yo,P,X,Y), 
-    some(X2,(Xo=P)*(Yo=X2)*(X=X2)*(Y='#e'))).
 
 program(agt,
         loop(if(-empty,
