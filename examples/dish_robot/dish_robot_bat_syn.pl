a2cda459c5044ca17096d34ad3b8ecbde9197c83
Jens Classen <classen@ruc.dk>
2024-12-19 14:10:14 +0100

    Renamed action 'requestDDR' to 'addDish' in dishwasher domain.

--- examples/dish_robot/dish_robot_bat_syn.pl
+++ examples/dish_robot/dish_robot_bat_syn.pl
@@ -28,20 +28,20 @@ rel_fluent(new(_)).
 rel_rigid(dish(_)).
 rel_rigid(room(_)).
 
-exo(requestDDR(_,_),true).
+exo(addDish(_,_),true).
 
 poss(load(X,Y),dirtyDish(X,Y)*at(Y)).
 poss(unload(X),onRobot(X)*at('#kitchen')).
-poss(requestDDR(X,Y),new(X)*room(Y)).
+poss(addDish(X,Y),new(X)*room(Y)).
 poss(goto(X),room(X)+(X='#kitchen')).
 
-causes_true(requestDDR(X,Y),dirtyDish(X,Y),true).
+causes_true(addDish(X,Y),dirtyDish(X,Y),true).
 causes_false(load(X,Y),dirtyDish(X,Y),true).
 
 causes_true(load(X,_Y),onRobot(X),true).
 causes_false(unload(X),onRobot(X),true).
 
-causes_false(requestDDR(X,_Y),new(X),true).
+causes_false(addDish(X,_Y),new(X),true).
 
 causes_true(goto(X),at(X),true).
 causes_false(goto(_Y),at(_X),true).
@@ -69,7 +69,7 @@ program(control,
 program(exog,
         star(pick(X,dish,
                   pick(Y,room,
-                       requestDDR(X,Y)))
+                       addDish(X,Y)))
             )
        ).
 
